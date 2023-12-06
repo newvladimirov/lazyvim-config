d053b5c5bae2888f62c1d9225b1beb1adbd752b2
Alex Vladimirov <alexey.vladimirov@team.wrike.com>
2023-12-06 18:21:59 +0100

    Add neorg basic setup

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,25 +1,46 @@
-return {
+-- lazy.nvim spec
+local M = {
   "nvim-neorg/neorg",
+  ft = "norg",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-cmp",
+    "nvim-lua/plenary.nvim",
+  },
   build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  event = "VeryLazy",
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.integrations.treesitter"] = {},
-        ["core.highlights"] = {},
-        ["core.mode"] = {},
-        ["core.qol.todo_items"] = {},
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-          },
-        },
-      },
-    })
-  end,
+  cmd = "Neorg",
 }
+
+local modules = {
+  ["core.defaults"] = {},
+  ["core.mode"] = {},
+  ["core.autocommands"] = {},
+  ["core.highlights"] = {},
+  ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+  ["core.qol.todo_items"] = {},
+  ["core.integrations.nvim-cmp"] = {},
+  ["core.integrations.treesitter"] = { config = { configure_parsers = true, install_parsers = true } },
+  ["core.concealer"] = { config = { icon_preset = "basic" } },
+  ["core.keybinds"] = {
+    -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+    config = {
+      default_keybinds = true,
+      neorg_leader = "\\",
+    },
+  },
+  ["core.dirman"] = {
+    config = {
+      workspaces = {
+        Personal = "~/_neorg/personal",
+        Work = "~/_neorg/work",
+      },
+    },
+  },
+}
+
+M.opts = {
+  load = modules,
+}
+
+return M
